80a9e5ca194c43e44176b721fc497fa6e84a51b5
OhKannaDuh <uimokd@gmail.com>
2025-06-11 13:21:14 +0100

    Added emergency stop button

--- OccultCrescentHelper/Windows/MainWindow.cs
+++ OccultCrescentHelper/Windows/MainWindow.cs
@@ -1,4 +1,7 @@
-﻿using ImGuiNET;
+﻿using Dalamud.Interface;
+using ImGuiNET;
+using OccultCrescentHelper.Modules.Automator;
+using Ocelot.IPC;
 using Ocelot.Windows;
 
 namespace OccultCrescentHelper.Windows;
@@ -8,6 +11,33 @@ public class MainWindow : OcelotMainWindow
 {
     public MainWindow(Plugin plugin, Config config)
         : base(plugin, config) { }
+
+    public override void PostInitialize()
+    {
+        base.PostInitialize();
+
+        TitleBarButtons.Add(new() {
+            Click = (m) => {
+                if (m != ImGuiMouseButton.Left)
+                {
+                    return;
+                }
+
+                if (plugin.modules.TryGetModule<AutomatorModule>(out var automator) && automator != null)
+                {
+                    automator.config.Enabled = false;
+                }
+
+                plugin.ipc.GetProvider<VNavmesh>()?.Stop();
+                Plugin.Chain.Abort();
+
+
+            },
+            Icon = FontAwesomeIcon.Stop,
+            IconOffset = new(2, 2),
+            ShowTooltip = () => ImGui.SetTooltip("Emergency Stop"),
+        });
+    }
 
     public override void Draw()
     {
