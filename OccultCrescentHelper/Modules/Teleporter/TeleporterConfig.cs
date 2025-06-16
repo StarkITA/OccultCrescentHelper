d68fe12e6777b1c5c8cad9a8fe66325a7b4fe424
OhKannaDuh <uimokd@gmail.com>
2025-06-16 15:00:07 +0100

    Removed unused config

--- OccultCrescentHelper/Modules/Teleporter/TeleporterConfig.cs
+++ OccultCrescentHelper/Modules/Teleporter/TeleporterConfig.cs
@@ -45,10 +45,4 @@ public class TeleporterConfig : ModuleConfig
     [Label("modules.teleporter.approach_aetheryte.label")]
     [Tooltip("modules.teleporter.approach_aetheryte.tooltip")]
     public bool ApproachAetheryte { get; set; } = false;
-
-    [Checkbox]
-    [RequiredPlugin("vnavmesh")]
-    [Label("modules.teleporter.refresh_buffs.label")]
-    [Tooltip("modules.teleporter.refresh_buffs.tooltip")]
-    public bool RefreshBuffs { get; set; } = false;
 }
